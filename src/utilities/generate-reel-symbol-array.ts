20ec14d7bd528ed9267a4c69da84265376bf6e07
Donald Mohlala <137010579+ByteBeem@users.noreply.github.com>
2023-12-03 14:13:58 +0000

    modified:   src/utilities/generate-reel-symbol-array.ts

--- src/utilities/generate-reel-symbol-array.ts
+++ src/utilities/generate-reel-symbol-array.ts
@@ -2,23 +2,23 @@ import type { ReelSymbol } from '@/typings';
 import { symbolData } from '../symbol-data';
 
 /**
- * Generates a shuffled array of ReelSymbol objects.
- * @returns Shuffled array of ReelSymbol objects.
+ * Creates a deliberately challenging array of ReelSymbol objects for players to navigate.
+ * @returns Challenging array of ReelSymbol objects.
  */
 export const generateReelSymbolArray = (): ReelSymbol[] => {
   const sortedSymbolData = [] as ReelSymbol[];
 
-  // Sort symbols based on value
+  // Sort symbols based on value in descending order
   symbolData.sort((a, b) => b.value - a.value);
 
-  // Calculate occurrence count for each symbol
+  // Calculate occurrence count for each symbol, making higher-value symbols rarer
   symbolData.forEach((sd, i) => {
     let occurrenceCount;
     if (i === 0 || i === 1) {
       occurrenceCount = 1;
     } else {
       // Increase the occurrence count for higher-value symbols to make them rarer
-      occurrenceCount = i * 3; // Increase this multiplier to make them even rarer
+      occurrenceCount = i * 3; // Increase this multiplier to amplify their rarity
     }
 
     // Uncomment the following line if you want to override count for a specific symbol
@@ -31,8 +31,8 @@ export const generateReelSymbolArray = (): ReelSymbol[] => {
     }
   });
 
-  // Perform multiple rounds of shuffling
-  const numRounds = 20; // Adjust the number of rounds based on desired difficulty
+  // Introduce complexity by performing multiple rounds of shuffling
+  const numRounds = 100; // Increase the number of rounds for heightened difficulty
   let shuffledArray = sortedSymbolData.slice(); // Initial clone
 
   for (let round = 0; round < numRounds; round++) {
@@ -43,14 +43,15 @@ export const generateReelSymbolArray = (): ReelSymbol[] => {
 };
 
 /**
- * Shuffles the input array using the Fisher-Yates algorithm.
+ * Applies the Fisher-Yates algorithm to shuffle the input array.
  * @param array - Array to be shuffled.
  * @returns Shuffled array.
  */
 const shuffleArray = (array: ReelSymbol[]): ReelSymbol[] => {
-  const clonedArray = [...array]; // Clone
+  const clonedArray = [...array]; // Clone the array
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
+    // Swap elements randomly to enhance unpredictability
     const temp = clonedArray[i];
     clonedArray[i] = clonedArray[j];
     clonedArray[j] = temp;
