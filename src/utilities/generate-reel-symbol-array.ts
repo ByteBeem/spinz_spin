49506912c0134f01a0e176285424e64ffedba5d7
Donald Mohlala <137010579+ByteBeem@users.noreply.github.com>
2023-12-15 09:23:48 +0200

    Update generate-reel-symbol-array.ts

--- src/utilities/generate-reel-symbol-array.ts
+++ src/utilities/generate-reel-symbol-array.ts
@@ -1,6 +1,36 @@
-
 import type { ReelSymbol } from '@/typings';
 import { symbolData } from '../symbol-data';
+
+/**
+ * Shuffles the input array randomly.
+ * @param array - Array to be shuffled.
+ * @returns Shuffled array.
+ */
+const shuffleArray = (array: ReelSymbol[]): ReelSymbol[] => {
+  const clonedArray = [...array]; // Clone the array
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    // Swap elements randomly to enhance unpredictability
+    const temp = clonedArray[i];
+    clonedArray[i] = clonedArray[j];
+    clonedArray[j] = temp;
+  }
+  return clonedArray;
+};
+
+/**
+ * Checks if the array has consecutive matching symbols.
+ * @param array - Array to be checked.
+ * @returns True if consecutive matching symbols are found, false otherwise.
+ */
+const hasConsecutiveMatches = (array: ReelSymbol[]): boolean => {
+  for (let i = 1; i < array.length; i++) {
+    if (array[i].name === array[i - 1].name) {
+      return true;
+    }
+  }
+  return false;
+};
 
 /**
  * Creates a deliberately challenging array of ReelSymbol objects for players to navigate.
@@ -19,7 +49,7 @@ export const generateReelSymbolArray = (): ReelSymbol[] => {
       occurrenceCount = 1;
     } else {
       // Increase the occurrence count for higher-value symbols to make them rarer
-      occurrenceCount = i * 3; // Increase this multiplier to amplify their rarity
+      occurrenceCount = i * 30; // Increase this multiplier to amplify their rarity
     }
 
     // Uncomment the following line if you want to override count for a specific symbol
@@ -32,43 +62,17 @@ export const generateReelSymbolArray = (): ReelSymbol[] => {
     }
   });
 
-  // Introduce complexity by performing multiple rounds of shuffling
-  const numRounds = 100; // Increase the number of rounds for heightened difficulty
   let shuffledArray = sortedSymbolData.slice(); // Initial clone
 
-  for (let round = 0; round < numRounds; round++) {
+  // Shuffle the array until there are no consecutive matching symbols
+  for (let round = 0; round < 100; round++) {
     shuffledArray = shuffleArray(shuffledArray);
-  }
 
-  return shuffledArray;
-};
-
-/**
- * Applies the Fisher-Yates algorithm to shuffle the input array,
- * ensuring that consecutive matching symbols do not occur.
- * @param array - Array to be shuffled.
- * @returns Shuffled array.
- */
-const shuffleArray = (array: ReelSymbol[]): ReelSymbol[] => {
-  const clonedArray = [...array]; // Clone the array
-
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-
-    // Swap elements randomly to enhance unpredictability
-    const temp = clonedArray[i];
-    clonedArray[i] = clonedArray[j];
-    clonedArray[j] = temp;
-
-    // Ensure that consecutive symbols are not the same
-    if (i > 0 && clonedArray[i].name === clonedArray[i - 1].name) {
-      // Swap with a non-consecutive element
-      const nonConsecutiveIndex = i - 2 >= 0 ? i - 2 : i + 1;
-      const tempNonConsecutive = clonedArray[i];
-      clonedArray[i] = clonedArray[nonConsecutiveIndex];
-      clonedArray[nonConsecutiveIndex] = tempNonConsecutive;
+    // If consecutive matches are found, reshuffle
+    if (!hasConsecutiveMatches(shuffledArray)) {
+      break;
     }
   }
 
-  return clonedArray;
+  return shuffledArray;
 };
