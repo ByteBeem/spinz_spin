99f9686d62c720c08ffa7653b13ba7f90ee429fb
Donald Mohlala <137010579+ByteBeem@users.noreply.github.com>
2023-12-15 08:59:26 +0200

    Update generate-reel-symbol-array.ts

--- src/utilities/generate-reel-symbol-array.ts
+++ src/utilities/generate-reel-symbol-array.ts
@@ -1,13 +1,45 @@
+User
 import type { ReelSymbol } from '@/typings';
 import { symbolData } from '../symbol-data';
 
 /**
- * Creates an array of ReelSymbol objects for players with only non-matching symbols.
- * @returns Array of ReelSymbol objects.
+ * Creates a deliberately challenging array of ReelSymbol objects for players to navigate.
+ * @returns Challenging array of ReelSymbol objects.
  */
 export const generateReelSymbolArray = (): ReelSymbol[] => {
-  const uniqueSymbolData = Array.from(new Set(symbolData));
-  const shuffledArray = shuffleArray(uniqueSymbolData);
+  const sortedSymbolData = [] as ReelSymbol[];
+
+  // Sort symbols based on value in descending order
+  symbolData.sort((a, b) => b.value - a.value);
+
+  // Calculate occurrence count for each symbol, making higher-value symbols rarer
+  symbolData.forEach((sd, i) => {
+    let occurrenceCount;
+    if (i === 0 || i === 1) {
+      occurrenceCount = 1;
+    } else {
+      // Increase the occurrence count for higher-value symbols to make them rarer
+      occurrenceCount = i * 3; // Increase this multiplier to amplify their rarity
+    }
+
+    // Uncomment the following line if you want to override count for a specific symbol
+    // if (sd.name === '1Cash') occurrenceCount -= 20;
+
+    // Add symbols to the sorted array based on occurrence count
+    while (occurrenceCount > 0) {
+      sortedSymbolData.push(sd);
+      occurrenceCount--;
+    }
+  });
+
+  // Introduce complexity by performing multiple rounds of shuffling
+  const numRounds = 100; // Increase the number of rounds for heightened difficulty
+  let shuffledArray = sortedSymbolData.slice(); // Initial clone
+
+  for (let round = 0; round < numRounds; round++) {
+    shuffledArray = shuffleArray(shuffledArray);
+  }
+
   return shuffledArray;
 };
 
