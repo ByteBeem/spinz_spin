cf05f366e63cd4e93041bc00ac2abe2ab82d7450
Donald Mohlala <137010579+ByteBeem@users.noreply.github.com>
2023-12-23 23:56:12 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -92,7 +92,8 @@ const urlSearchParams = new URLSearchParams(window.location.search);
       }
     })
     .then(userDataResponse => {
-      console.log('User data from server:', userDataResponse.data[0);
+      console.log('User data from server:', userDataResponse.data[0]);
+
       const userData = userDataResponse.data[0];
       
       // Set the retrieved balance to this.credits
